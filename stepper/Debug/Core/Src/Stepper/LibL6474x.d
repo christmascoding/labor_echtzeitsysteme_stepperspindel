337a0edadf4f25bb16bf7d2d5d9beabc06dcc393
christmascoding <jonas.muenz@protonmail.com>
2025-04-17 10:23:22 +0200

    initial

---
+++ stepper/Debug/Core/Src/Stepper/LibL6474x.d
@@ -0,0 +1,6 @@
+Core/Src/Stepper/LibL6474x.o: \
+ C:/Users/hans-/Nextcloud/Documents/DHBW/Labor/Labor_HWNahesProg_Echtzeitsysteme/Semester4/Solution/libs/LibL6474/src/LibL6474x.c \
+ ../../libs/LibL6474/inc/LibL6474.h \
+ C:/Users/hans-/Nextcloud/Documents/DHBW/Labor/Labor_HWNahesProg_Echtzeitsysteme/Semester4/Solution/stepper/Core/Inc/Stepper/LibL6474Config.h
+../../libs/LibL6474/inc/LibL6474.h:
+C:/Users/hans-/Nextcloud/Documents/DHBW/Labor/Labor_HWNahesProg_Echtzeitsysteme/Semester4/Solution/stepper/Core/Inc/Stepper/LibL6474Config.h:
