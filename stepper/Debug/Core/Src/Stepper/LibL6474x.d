1e40d355a8950e45af154a4cad2a52e279cf12fc
christmascoding <jonas.muenz@protonmail.com>
2025-05-15 09:46:57 +0200

    Remove debug artifacts and ignore them

--- stepper/Debug/Core/Src/Stepper/LibL6474x.d
+++
@@ -1,6 +0,0 @@
-Core/Src/Stepper/LibL6474x.o: \
- /home/carl/Documents/Uni/Spindel_Project_Hardwarenahe_Programmierung_von_Echtzeitsystemen/labor_echtzeitsysteme_stepperspindel/libs/LibL6474/src/LibL6474x.c \
- ../../libs/LibL6474/inc/LibL6474.h \
- /home/carl/Documents/Uni/Spindel_Project_Hardwarenahe_Programmierung_von_Echtzeitsystemen/labor_echtzeitsysteme_stepperspindel/stepper/Core/Inc/Stepper/LibL6474Config.h
-../../libs/LibL6474/inc/LibL6474.h:
-/home/carl/Documents/Uni/Spindel_Project_Hardwarenahe_Programmierung_von_Echtzeitsystemen/labor_echtzeitsysteme_stepperspindel/stepper/Core/Inc/Stepper/LibL6474Config.h:
